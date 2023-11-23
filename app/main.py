a0662fe2e55348bcd3b21dbf2fc3df348b43d587
KLINTez <KLINTez@gitlab.com>
2023-11-23 10:24:40 +0300

    cpmmit! user can't inter task more then 500 symbols

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 """
 from loguru import logger
 
-from fastapi import FastAPI, Request, Depends, Form, status, Response
+from fastapi import FastAPI, Request, Depends, Form, status, Response, Query
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -34,7 +34,10 @@ async def home(request: Request, database: Session = Depends(get_db)):
 
 
 @app.post("/add", status_code=status.HTTP_201_CREATED)
-async def todo_add(request: Request, response: Response, task: Annotated[str, Form()] = None, database: Session = Depends(get_db)):
+async def todo_add(request: Request,
+                   response: Response,
+                   task: Annotated[str, Form(max_length=500)] = None,
+                   database: Session = Depends(get_db)):
     """Add new todo
     """
     if task is None or task.replace(" ", "") == "" or task == "":
@@ -48,7 +51,8 @@ async def todo_add(request: Request, response: Response, task: Annotated[str, Fo
 
 
 @app.get("/edit/{todo_id}")
-async def todo_get(request: Request, todo_id: int, database: Session = Depends(get_db)):
+async def todo_get(request: Request,
+                   todo_id: int, database: Session = Depends(get_db)):
     """Get todo
     """
     todo = database.query(models.Todo).filter(models.Todo.id == todo_id).first()
@@ -56,15 +60,19 @@ async def todo_get(request: Request, todo_id: int, database: Session = Depends(g
     return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
 
 
-@app.post("/edit/{todo_id}")
+@app.post("/edit/{todo_id}", status_code=status.HTTP_202_ACCEPTED)
 async def todo_edit(
         request: Request,
+        response: Response,
         todo_id: int,
-        task: str = Form(...),
+        task:  Annotated[str, Form(max_length=500)] = None,
         completed: bool = Form(False),
         database: Session = Depends(get_db)):
     """Edit todo
     """
+    if task is None or task.replace(" ", "") == "":
+        response.status_code = status.HTTP_411_LENGTH_REQUIRED
+        return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
     todo = database.query(models.Todo).filter(models.Todo.id == todo_id).first()
     logger.info(f"Editting todo: {todo}")
     todo.task = task
@@ -74,7 +82,9 @@ async def todo_edit(
 
 
 @app.get("/delete/{todo_id}")
-async def todo_delete(request: Request, todo_id: int, database: Session = Depends(get_db)):
+async def todo_delete(request: Request,
+                      todo_id: int,
+                      database: Session = Depends(get_db)):
     """Delete todo
     """
     todo = database.query(models.Todo).filter(models.Todo.id == todo_id).first()
