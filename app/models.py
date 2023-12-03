71689a0514bb4a6c7c6a46750384f54ccc0942ef
KLINTez <KLINTez@gitlab.com>
2023-12-03 17:35:37 +0300

    commit! add fields date_creation and date_completion to db model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 """
 from sqlalchemy import Column, Integer, Boolean, Text
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import date
 
 from tags import TodoTags
 
@@ -17,6 +18,8 @@ class Todo(Base):
     details = Column(Text)
     completed = Column(Boolean, default=False)
     type = Column(Text)
+    date_creation = Column(Text, details="date of creation", default=date.today())
+    date_completion = Column(Text, details="date of completion", default=None)
 
     def __repr__(self):
         return f'<Todo {self.id}>'
