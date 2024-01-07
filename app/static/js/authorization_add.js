bc13b361d17f59e8370213f137fe20e27a2eedb1
KLINTez <klintez@gitlab.com>
2024-01-07 04:10:37 +0300

    commit! listener add todo

---
+++ app/static/js/authorization_add.js
@@ -0,0 +1,27 @@
+
+const blogPostBtn = document.querySelector('#add_new_todo')
+
+blogPostBtn.addEventListener('click', () => {
+    console.log(document.getElementById("title_add").value)
+    formData = new FormData()
+    formData.append("title", document.getElementById('title_add').value)
+    formData.append("details", document.getElementById('details_add').value)
+    formData.append("type", document.getElementById('type').value)
+    formData.append("fullname", document.getElementById('fullname').value)
+    fetch(`/add`, {
+        method: "POST",
+        body: formData,
+        headers: {
+            // Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${localStorage.getItem('token') }`
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data["answer"] == "title not found")
+                console.log(data);
+            else
+                window.location.href = "/list";
+        })
+        .catch(error => console.error(error))
+})
