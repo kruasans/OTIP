c8cf5d6bdad2cd51296eb23f4c7291e1ae139b12
KLINTez <klintez@gitlab.com>
2024-01-07 16:12:28 +0300

    commit! chenge listener to function in index for add_todo

--- app/static/js/authorization.js
+++ app/static/js/authorization.js
@@ -1,9 +1,5 @@
-
-const blogPostBtn = document.querySelector('#add_new_todo')
-
-blogPostBtn.addEventListener('click', () => {
-    console.log(document.getElementById("title_add").value)
-    formData = new FormData()
+function add_todo(){
+formData = new FormData()
     formData.append("title", document.getElementById('title_add').value)
     formData.append("details", document.getElementById('details_add').value)
     formData.append("type", document.getElementById('type').value)
@@ -12,21 +8,29 @@ blogPostBtn.addEventListener('click', () => {
         method: "POST",
         body: formData,
         headers: {
-            // Authorization: `Bearer ${token}`
             Authorization: `Bearer ${localStorage.getItem('token') }`
         },
     })
-        .then(response => response.json())
+        .then(response => {
+            if(response.status==401)
+                throw new Error(response.status);
+            return response.json();
+        })
         .then(data => {
             if(data["answer"] == "title not found"){
                 console.log(data);
             }
-            else if(data["answer"] == "login"){
-                window.location.href = "/log_in";
-            }
             else if(data["answer"] == "good"){
                 window.location.href = "/list";
             }
+
         })
-        .catch(error => console.error(error))
-})
+        .catch(error => {
+            if(error.message == "401"){
+                alert("Ошибка авторизации");
+                window.location.href = "/log_in";
+            }else{
+                console.error('Произошла ошибка:', error);
+            }
+        })
+        }
