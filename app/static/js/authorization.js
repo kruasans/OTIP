488fbc3ad09f5d9d60239429236a5fc72bd60d8f
KLINTez <klintez@gitlab.com>
2024-01-12 16:15:48 +0300

    commit! refactoring

--- app/static/js/authorization.js
+++ app/static/js/authorization.js
@@ -163,7 +163,7 @@ function edit(todo_id){
     formData = new FormData();
     formData.append("title", document.getElementById('title').value);
     formData.append("details", document.getElementById('details').value);
-    formData.append("completed", document.getElementById('completed').value);
+    formData.append("completed", document.getElementById('completed').checked);
     formData.append("fullname", document.getElementById('fullname').value);
     fetch(path, {
         method: "POST",
@@ -198,45 +198,6 @@ function edit(todo_id){
         })
 }
 
-function edit(todo_id){
-    var path = "/edit/" + todo_id;
-    formData = new FormData();
-    formData.append("title", document.getElementById('title').value);
-    formData.append("details", document.getElementById('details').value);
-    formData.append("completed", document.getElementById('completed').value);
-    formData.append("fullname", document.getElementById('fullname').value);
-    fetch(path, {
-        method: "POST",
-        body: formData,
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token') }`
-        },
-    })
-        .then(response => {
-            console.log("response");
-            console.log(response);
-            if(response.status!=200)
-                throw new Error(response.status);
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            window.location.href = "/list";
-        })
-        .catch(error => {
-            if(error.message == "401"){
-                alert("Ошибка авторизации");
-                window.location.href = "/log_in";
-            }else if(error.message == "422"){
-                alert("Неверный формат данных");
-                window.location.href = "/list";
-            }else{
-                console.error('Произошла ошибка:', error);
-                alert("Ошибка");
-                window.location.href = "/log_in";
-            }
-        })
-}
 
 function load_image(todo_id){
     var path = "/load_image/" + todo_id;
@@ -272,9 +233,14 @@ function load_image(todo_id){
                 alert("Неверный формат картинки");
                 location.reload();
             }else{
+                if(error.message == "409"){
+                    alert("Выбран дубликат");
+                    location.reload();
+                }else{
                 console.error('Произошла ошибка:', error);
                 alert("Ошибка");
                 window.location.href = "/";
+                }
             }
         })
 }
