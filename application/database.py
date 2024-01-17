e92241fe5f2bfc93b66132e8b1f8be3d72c22fbf
KLINTez <klintez@gitlab.com>
2024-01-17 21:40:14 +0300

    commit! new database realization

--- application/database.py
+++ application/database.py
@@ -2,21 +2,17 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session  #pylint: disable=unused-import
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session  # pylint: disable=unused-import
 
-from models import Base
-
-DB_URL = "sqlite:///./db.sqlite"
+DB_URL = "sqlite:///./application/db.sqlite"
 ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False})
 # for logging all SQL-queries
-#ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False}, echo=True)
+# ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False}, echo=True)
 SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
 
-def init_db():
-    """Init database, create all models as tables
-    """
-    # check_same_thread is for SQLite only
-    Base.metadata.create_all(bind=ENGINE)
+Base = declarative_base()
+
 
 def get_db():
     """Create session/connection for each request
